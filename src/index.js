842db395c607c3019d2630098fa1f1e94802420b
Mishochik <mixailoparin@gmail.com>
2020-02-28 16:22:35 +0500

    Исправлены запросы, добавлена фильтрация списка аэропортов

--- src/index.js
+++ src/index.js
@@ -3,40 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import axios from "axios";
 
+const api = axios.create({
+    "headers": {
+        "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
+        "x-rapidapi-key": "aae212215dmsh0aeb91f7306b2ddp1f8d34jsn2baf847ae58d"
+    }
+});
+
+
 function getInitialAirports() {
-    return axios.get(
-        'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=6000&lng=57.915054&lat=56.010555',
-        {
-            "headers": {
-                "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
-                "x-rapidapi-key": "aae212215dmsh0aeb91f7306b2ddp1f8d34jsn2baf847ae58d"
-            }
-        }
+    return api.get(
+        'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=6000&lng=57.915054&lat=56.010555'
     )
 }
 
 function getAirportsByName(params) {
-    return axios.get(
-        'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=' + params.text,
-        {
-            "headers": {
-                "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
-                "x-rapidapi-key": "aae212215dmsh0aeb91f7306b2ddp1f8d34jsn2baf847ae58d"
-            }
-        }
+    return api.get(
+        'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=' + params.text
     )
 }
 
 function getAirportsByCode(params) {
-    return axios.get(
-        'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-code?code=' + params.text,
-        {
-            "headers": {
-                "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
-                "x-rapidapi-key": "aae212215dmsh0aeb91f7306b2ddp1f8d34jsn2baf847ae58d"
-            }
-        }
+    return api.get(
+        'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-code?code=' + params.text
     )
+}
+
+function unique(arr, key = 'airportId') {
+    const result = [];
+
+    function itemCheck(item) {
+        if (result.indexOf(item[key]) === -1) {
+            result.push(item[key]);
+            return true
+        }
+
+        return false;
+    }
+
+    return arr.filter(item => itemCheck(item));
 }
 
 class App extends React.Component {
@@ -45,32 +50,36 @@ class App extends React.Component {
         this.state = {
             data: [],
             filterText: '',
-            loading: true,
         };
         this.search = this.search.bind(this);
         this.getAirports = this.getAirports.bind(this);
     }
 
-    componentDidMount() {
-        getInitialAirports().then(response => {
-            const data = response.data;
-            this.setState({data});
-        })
+    async componentDidMount() {
+        const response = await getInitialAirports();
+        const data = response.data;
+        this.setState({data});
     }
 
     search(filterText) {
         this.setState({filterText});
     }
 
+    async getAirports() {
+        if (this.state.filterText.length === 0) {
+            return
+        }
 
-    getAirports() {
-        axios.all([getAirportsByName({text: this.state.filterText}), getAirportsByCode({text: this.state.filterText})])
-            .then(axios.spread((airportsByName, airportsByCode) => {
-                if (airportsByName.data.length !== 0 || airportsByCode.data.length !== 0) {
-                    const data = this.state.data.concat(airportsByName.data, airportsByCode.data);
-                    this.setState({data});
-                }
-            }));
+        let airportsByCode = {data: []};
+        if (this.state.filterText.length < 4) {
+            airportsByCode = await getAirportsByCode({text: this.state.filterText});
+        }
+
+        const airportsByName = await getAirportsByName({text: this.state.filterText});
+        if (airportsByName.data.length !== 0 || airportsByCode.data.length !== 0) {
+            const data = unique(this.state.data.concat(airportsByName.data, airportsByCode.data));
+            this.setState({data});
+        }
     }
 
     render() {
